27b79342a590cfcab066c339a22d1a4a20d055ad
Luke Oliff <luke@lukeoliff.com>
2024-05-17 12:58:12 +0100

    feat: adding event listeners for relevant events (#8)
    * feat: adding event listeners for relevant events
    
    * feat: adding event listeners for relevant events
    
    ---------
    
    Co-authored-by: Vaibhav Rai <raivaibhav08@gmail.com>

---
+++ src/events/action.types.ts
@@ -0,0 +1,8 @@
+export const enum ACTION {
+  THREAD_DELETE = "thread_delete",
+  THREAD_UPDATE = "thread_update",
+  THREAD_CREATE = "thread_create",
+  MESSAGE_CREATE = "message_create",
+  MESSAGE_UPDATE = "message_update",
+  MESSAGE_DELETE = "message_delete",
+}
