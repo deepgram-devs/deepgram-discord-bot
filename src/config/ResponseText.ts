3fcc9c029e17d700d78977e8aadadd03d7b2f6bc
John Vajda (JV) <john.vajda@deepgram.com>
2024-05-24 10:22:36 -0600

    updates sticky message (#11)
    * updates sticky message
    
    * resolves failed lint check

--- src/config/ResponseText.ts
+++ src/config/ResponseText.ts
@@ -30,4 +30,4 @@ export enum ResponseText {
 /**
  * This is separate so that the environment value can be interpolated.
  */
-export const StickyMessage = `A friendly reminder to everyone that questions should be asked in <#${process.env.HELP_CHANNEL_ID}>.`;
+export const StickyMessage = `We welcome discussions in this channel, but for technical help, please use <#${process.env.HELP_CHANNEL_ID}>.`;
